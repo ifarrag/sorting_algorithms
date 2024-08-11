e617e93fb41d5111f862b84e9efe5f9db01dafb0
ifarrag <ifarrrag@gmail.com>
2024-08-11 00:10:27 -0700

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stdbool.h>
+/**
+ * bubble_sort - Sort array in ascending
+ * array: Pointer to array to sort
+ * size: The size to sort in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 1;
+	int temp = 0;
+	bool sw = 0;
+
+	while (!array)
+	{
+		if (i == size && sw == 1)
+		{
+			i = 1;
+			sw = 0;
+		}
+		if (i == size && sw == 0)
+			break;
+		if (array[i - 1] > array[i])
+		{
+			sw = 1;
+			temp = array[i - 1];
+			array[i - 1] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
