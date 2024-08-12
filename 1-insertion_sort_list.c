be677b08861ba85e55de8a72e3872be8e3320d57
ifarrag <ifarrrag@gmail.com>
2024-08-11 21:31:48 -0700

    Insertion sort algorithm with list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * @list: Pointer to pointer to list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tempn, *tempp;
+	listint_t *nlist = *list;
+
+	while (nlist->next)
+	{
+		nlist = nlist->next;
+		while (nlist->n < nlist->prev->n)
+		{
+			tempn = nlist->next;
+			tempp = nlist->prev;
+			if (tempn)
+				tempn->prev = tempp;
+			nlist->next = nlist->prev;
+			nlist->prev = nlist->prev->prev;
+			tempp->next = tempn;
+			tempp->prev = nlist;
+			if (nlist->prev)
+				nlist->prev->next = nlist;
+			print_list(*list);
+		}
+	}
+}
