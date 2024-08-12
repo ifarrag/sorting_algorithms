12679e6a019ba58aa427418e2658fa01335277a1
ifarrag <ifarrrag@gmail.com>
2024-08-12 00:14:52 -0700

    Selection sort algorithm with array

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stdbool.h>
+
+/**
+ * selection_sort - sorts an array of integers
+ * @array: Target array
+ * @size: range
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 1, y = 0;
+	int temp = 0;
+	bool val = false;
+
+	while(array)
+	{
+		while (i < size)
+		{
+			if (array[y] > array[i])
+			{
+				temp = array[i];
+				val = true;
+			}
+			i++;
+		}
+		if (val == true)
+		{
+			val = false;
+			array[y] = temp;
+			print_array(array);
+		}
+		y++;
+	}
+}
